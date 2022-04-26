220ca0472ff68c3118e203bc54002e9523a646c7
Mahmoud Magdy Hegazi <55125302+MahmoudHegazi@users.noreply.github.com>
2022-04-26 11:17:15 +0200

    Update cal.py

--- cal.py
+++ cal.py
@@ -4,12 +4,12 @@ def cal_data(start_year=1920, years_limit=100, need='all'):
     data = {'days': [], 'month_names': [], 'years': []}
     accepted_needs = ['days','month_names','years']
     for i in range(1,13):
-        data['days'].append(('day-' + str(i),str(i)))
-        
+        data['days'].append((str(i),str(i)))
+
         datetime_month_object = datetime.datetime.strptime(str(i), "%m")
         month_name_s = datetime_month_object.strftime("%b")
-        month_name_c = datetime_month_object.strftime("%B")
-        data['month_names'].append((month_name_s,month_name_c))
+        month_name_f = datetime_month_object.strftime("%B")
+        data['month_names'].append((str(i),month_name_f))
 
     if not str(start_year).isnumeric():
         start_year = 1920
@@ -19,15 +19,14 @@ def cal_data(start_year=1920, years_limit=100, need='all'):
         years_limit = 100
     else:
         years_limit = int(years_limit)
-    
+
     for y in range(start_year, start_year + years_limit):
-        data['years'].append(('year-' + str(y), str(y)))
-    
+        data['years'].append((str(y), str(y)))
+
     if need in accepted_needs:
         return data[need]
     else:
         return data
-
 print(cal_data(start_year=1801, years_limit=200,need='days'))
 print(cal_data(start_year=1801, years_limit=200,need='month_names'))
 print(cal_data(start_year=1801, years_limit=200,need='years'))
